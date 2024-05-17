89525b868adcefb10f9eb12f38fbb2db18c7cc8e
HijiriSato88 <satohiji0808@outlook.com>
2024-05-17 11:54:14 +0900

    適当にtest

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 user = User.create!(user_id: 'test123', password: 'password')
 
 status_data = {
@@ -13,3 +14,5 @@ status_data.each do |status_id, status_name|
   status = Status.create!(status_id: status_id, status_name: status_name)
   Schedule.create!(user_id: user.user_id, status_id: status.status_id)
 end
+
+#test
