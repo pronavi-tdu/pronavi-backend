f806120c6f632a8257fb2d4bf671758b04b0802b
koichi <111332153+koichi2426@users.noreply.github.com>
2024-06-12 17:44:19 +0900

    開発環境でwww.pronavi.onlineのホスト許可を追加
    開発環境でwww.pronavi.onlineのホスト許可を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Enable DNS rebinding protection and other `Host` header attacks.
+  config.hosts << "www.pronavi.online"
 end
